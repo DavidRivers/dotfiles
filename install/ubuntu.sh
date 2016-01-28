b77b904f9edff9e55d659ad16e7c5782f797ad28
David Rivers <david@davidrivers.name>
2016-01-28 17:03:31 -0500

    Add apt-get installation of system dependencies first

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Install system dependencies
+sudo apt-get install git fish
+
+# Symlink configuration files
 mkdir --parents ~/.config/fish
-
 ln --symbolic ~/Dev/dotfiles/.tmux.conf ~/.tmux.conf
-
 ln --symbolic ~/Dev/dotfiles/config.fish ~/.config/fish/
 ln --symbolic ~/Dev/dotfiles/aliases.fish ~/.config/fish/aliases.fish
-
 ln --symbolic ~/Dev/dotfiles/.vimrc ~/.vimrc
+
+# Install vim dependencies
