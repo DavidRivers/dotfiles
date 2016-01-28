7c37a5a17a1aaab6ba158b478e85d1062cb8951e
David Rivers <david@davidrivers.name>
2016-01-28 17:11:54 -0500

    Add vim dependencies (pathogen, fugitive, solarized

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -11,3 +11,8 @@ ln --symbolic ~/Dev/dotfiles/aliases.fish ~/.config/fish/aliases.fish
 ln --symbolic ~/Dev/dotfiles/.vimrc ~/.vimrc
 
 # Install vim dependencies
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+  curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/
+git clone git://github.com/tpope/vim-fugitive.git ~/.vim/bundle/ && \
+  vim -u NONE -c "helptags vim-fugitive/doc" -c q
