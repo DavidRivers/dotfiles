dbab83b1487d9c798fd1eeabf4e15178358f0f47
David Rivers <david@davidrivers.name>
2016-05-15 15:14:30 -0400

    Automatically install pathogen (vim plugin); add more notes

--- install/mac.sh
+++ install/mac.sh
@@ -1,32 +1,47 @@
 #!/usr/bin/env bash
 # https://github.com/scooter-dangle/dotfiles/blob/master/build_env.sh
 
-# userland utils
+## Need to automate association of GitHub SSH identity
+# 1: check if there is an ssh identity
+  # YES: prompt whether to use this identity for cloning ~/Dev dependencies from GitHub
+  # NO: prompt user to create a new ssh identity
+  # 2: upload new (or pre-existing) ssh identity to github settings
+# 3: save name and email to global git config
+# 4: use ssh identity to clone dotfiles (and save to ssh-agent, etc., if necessary)
+
+## userland utils
 
 brew install coreutils fish tmux git vim
 brew cask install flux divvy firefox google-chrome vlc
 
-# Terminal.app configs
+## Terminal.app configs
 
 open -b 'com.apple.Terminal' "https://raw.githubusercontent.com/tomislav/osx-terminal.app-colors-solarized/master/Solarized%20Dark.terminal"
 open -b 'com.apple Terminal' "https://raw.githubusercontent.com/tomislav/osx-terminal.app-colors-solarized/master/Solarized%20Light.termina://raw.githubusercontent.com/tomislav/osx-terminal.app-colors-solarized/master/Solarized%20Light.terminal"
 
-# fish configs
+## pull dotfiles
+
+mkdir -p ~/Dev
+git clone https://github.com/DavidRivers/dotfiles ~/Dev/dotfiles
+ln -s ~/Dev/dotfiles/.vimrc ~/.vimrc
+mkdir -p ~/.config/fish/
+ln -s ~/Dev/dotfiles/config.fish ~/.config/fish/
+ln -s ~/Dev/dotfiles/aliases.fish ~/.config/fish/aliases.fish
+
+## fish configs
 
 mkdir -p ~/.config/fish/completions
 curl https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.fish --output ~/.config/fish/completions/tmuxinator.fish
-ln -s ~/dotfiles/config.fish ~/.config/fish/
-ln -s ~/dotfiles/aliases.fish ~/.config/fish/aliases.fish
 
-# vim config
+## vim config
 
+git clone git://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive && vim -u NONE -c "helptags vim-fugitive/doc" -c q
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
   curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-git clone https://github.com/DavidRivers/dotfiles ~/dotfiles
-ln -s ~/dotfiles/.vimrc ~/.vimrc
 git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
 
 
-# launch windows (start workflow)
+## launch windows (start workflow)
+
 open -a Firefox && osascript -e 'tell application "System Events" to keystroke "h" using {command down, option down}'
 open -a Terminal && osascript -e 'tell application "System Events" to keystroke "l" using {command down, option down, control down}'
